142b57f9aefff7b33def15a89f3035e7b29f26a8
rutuja-ganorkar21 <rutujaganorkar35@gmail.com>
2026-05-21 13:22:06 +0530

    fix CORS preflight

--- src/index.js
+++ src/index.js
@@ -15,7 +15,9 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-  }),
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 
 app.use(express.json());
